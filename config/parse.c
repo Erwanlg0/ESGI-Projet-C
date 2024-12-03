5a49ed4aa58f932bb937117b8fe536ce3c525531
ayoub <ayoubousbia02@gmail.com>
2024-12-03 10:22:43 +0100

    add parsing for langugae (retrieval + check validity)

--- config/parse.c
+++ config/parse.c
@@ -1,4 +1,5 @@
 #include "parse.h"
+#include <stdio.h>
 #include <string.h>
 
 char *file_to_buffer(FILE *file, char *buffer) {
@@ -10,4 +11,31 @@ char *file_to_buffer(FILE *file, char *buffer) {
   }
 
   return buffer;
+}
+
+char *is_language(char *buffer, char language[3]) {
+  int n;
+  char *line = strtok(buffer, "\n");
+
+  while (line != NULL) {
+    if (strstr(line, "Language =") == line) {
+      n = sscanf(line, "Language = %2s", language);
+    }
+
+    line = strtok(NULL, "\n");
+  }
+
+  return n == 1 ? language : NULL;
+}
+
+int is_valid_language(char *language) {
+  char list_lan[2][2] = {"fr", "en"};
+  int count;
+
+  for (int i = 0; i < 2; i++) {
+    if (language == list_lan[i]) {
+      count = 1;
+    }
+  }
+  return count == 1 ? 0 : 1;
 }
--- config/parse.h
+++ config/parse.h
@@ -7,5 +7,7 @@
 #define MAX_BUFFER_SIZE 10000
 
 char *file_to_buffer(FILE *file, char *buffer);
+char *is_language(char *buffer, char language[3]);
+int is_valid_language(char *language);
 
-#endif // !DBUG
+#endif
