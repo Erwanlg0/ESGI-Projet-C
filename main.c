1cf3ed848d6e634dd942f95389861fabdcd61d78
ayoub <ayoubousbia02@gmail.com>
2024-12-03 10:23:22 +0100

    add test function for parsing

--- main.c
+++ main.c
@@ -4,10 +4,18 @@ int main() {
 
   FILE *file = fopen("text.txt", "r");
   char *buffer = malloc(sizeof(char) * MAX_BUFFER_SIZE);
+  char *language = malloc(sizeof(char) * 3);
 
   if (file == NULL)
     return 1;
 
   buffer = file_to_buffer(file, buffer);
   printf("%s", buffer);
+
+  language = is_language(buffer, language);
+  if (is_valid_language(language) == 1) {
+    printf("Langue non prise en compte");
+    return 1;
+  }
+  printf("%s", language);
 }
