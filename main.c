ffec34abcb04c4933833329df371c72988fb58b0
ayoub <ayoubousbia02@gmail.com>
2024-12-09 19:08:12 +0100

    add difficulty to function call (check_config)

--- main.c
+++ main.c
@@ -5,11 +5,12 @@ int main() {
   FILE *file = fopen("text.txt", "r");
   char *language = malloc(sizeof(char) * 3);
   char *music = malloc(sizeof(char) * 20);
+  char *difficulty = malloc(sizeof(char) * 6);
 
   if (file == NULL)
     return 1;
 
-  int res = check_config(file, language, music);
+  int res = check_config(file, language, difficulty, music);
   if (!res) {
     exit(0);
   }
