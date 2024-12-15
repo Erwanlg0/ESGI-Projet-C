e57cb958d249a3e26507dd5a6d553cd153502b5f
ayoub <ayoubousbia02@gmail.com>
2024-12-15 13:48:14 +0100

    add size window error handling

--- main.c
+++ main.c
@@ -2,16 +2,19 @@
 
 int main() {
 
-  FILE *file = fopen("text.txt", "r");
   char *language = malloc(sizeof(char) * 3);
   char *music = malloc(sizeof(char) * 20);
   char *difficulty = malloc(sizeof(char) * 6);
+  int *width = malloc(sizeof(int));
+  int *height = malloc(sizeof(int));
+  FILE *file = fopen("text.txt", "r");
 
   if (file == NULL)
     return 1;
 
   int res = check_config(file, language, difficulty, music);
-  if (!res) {
+  int error = check_win_size(file, width, height);
+  if (!error) {
     exit(0);
   }
 }
