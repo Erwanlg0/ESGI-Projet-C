5e875a890218b40b3353a3502f676a3b8e72f9e3
ayoub <ayoubousbia02@gmail.com>
2024-12-15 13:51:51 +0100

    remove file manipulation (handled in parsin branch)

--- main.c
+++ main.c
@@ -1,20 +1,8 @@
-#include "config/parse.h"
 #include "config/config.h"
 
-int main()
-{
+int main() {
   Settings settings;
-  if (!LoadSettings(&settings, "config/settings.cfg"))
-  {
+  if (!LoadSettings(&settings, "config/settings.cfg")) {
     return 1;
   }
-
-  FILE *file = fopen("text.txt", "r");
-  char *buffer = malloc(sizeof(char) * MAX_BUFFER_SIZE);
-
-  if (file == NULL)
-    return 1;
-
-  buffer = file_to_buffer(file, buffer);
-  printf("%s", buffer);
 }
